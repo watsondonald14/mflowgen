d8e2b4130e49ec3e302815705f76139c0684c5d6
Livan <pomarburentheo@outlook.com>
2026-01-02 22:01:03 +0300

    update

---
+++ mflowgen/tests/test_subgraph/dummy_double_passthrough/construct.py
@@ -0,0 +1,83 @@
+#=========================================================================
+# construct.py
+#=========================================================================
+# Dummy graph for testing graph construction with Subgraphs. Contains
+# Passthough subgraph that fans out input to 2 passthrough nodes to test
+# Subgraph input fanout.
+#
+# Author : Alex Carsello
+# Date   : Oct. 4, 2023
+#
+
+import os
+
+from mflowgen.components import Graph, Node, Subgraph
+
+def construct():
+
+  g = Graph()
+
+  #-----------------------------------------------------------------------
+  # Parameters
+  #-----------------------------------------------------------------------
+
+  adk_name = 'freepdk-45nm'
+  adk_view = 'view-tiny'
+
+  parameters = {
+    'construct_path' : __file__,
+    'adk'            : adk_name,
+    'adk_view'       : adk_view,
+  }
+
+  #-----------------------------------------------------------------------
+  # Create nodes
+  #-----------------------------------------------------------------------
+
+  this_dir = os.path.dirname( os.path.abspath( __file__ ) )
+
+  # ADK node
+
+  g.set_adk( adk_name )
+  adk = g.get_adk_node()
+
+  # Custom nodes
+
+  dummy_input  = Node( this_dir + '/../dummy/dummy_input'  )
+  dummy_output = Node( this_dir + '/../dummy/dummy_output' )
+
+  # Subgraphs
+
+  passthrough_subgraph = Subgraph( this_dir + '/../double_passthrough', 'double_passthrough' )
+
+  # Default nodes
+
+  info    = Node( 'info',                 default=True )
+
+  #-----------------------------------------------------------------------
+  # Graph -- Add nodes
+  #-----------------------------------------------------------------------
+
+  g.add_node( info                 )
+  g.add_node( dummy_input          )
+  g.add_node( passthrough_subgraph )
+  g.add_node( dummy_output         )
+
+  #-----------------------------------------------------------------------
+  # Graph -- Add edges
+  #-----------------------------------------------------------------------
+
+  g.connect_by_name( dummy_input, passthrough_subgraph  )
+  g.connect_by_name( passthrough_subgraph, dummy_output )
+
+  #-----------------------------------------------------------------------
+  # Parameterize
+  #-----------------------------------------------------------------------
+
+  g.update_params( parameters )
+
+  return g
+
+if __name__ == '__main__':
+  g = construct()
+
